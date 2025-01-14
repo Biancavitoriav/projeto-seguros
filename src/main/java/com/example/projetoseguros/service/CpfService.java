bf608635ccec068a5d102057b11a41dde086069f
Biancavitoriav <bianca.veloso@germinare.org.br>
2025-01-14 19:38:00 -0300

    Mudando o metodo de muliplicacao

--- src/main/java/com/example/projetoseguros/service/CpfService.java
+++ src/main/java/com/example/projetoseguros/service/CpfService.java
@@ -4,25 +4,36 @@ import com.example.projetoseguros.repositories.CpfRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
-import java.util.Arrays;
-import java.util.List;
-
 @Service
 @AllArgsConstructor
 public class CpfService {
+
     private CpfRepository cpfRepository;
+
     public String validCpf(String cpf) {
-        boolean erro = false;
+        //validating cpf manually
+        boolean error = false;
+        int multiplier = 0;
+        String listaImprimida = "";
+
         if (cpf.length() != 11) {
-            erro = true;
+            error = true;
         }
+
+        //inserting each cpf element in a list
         String[] cpfList = new String[cpf.length()];
         for (int i = 0; i < cpf.length(); i++) {
             cpfList[i] = String.valueOf(cpf.charAt(i));
         }
-        for (int i = 2; i < cpfList.length; i++) {
-            cpfList[i] = String.valueOf(Integer.parseInt(Arrays.toString(cpfList))*2);
+
+        //multiplying the numbers,
+        //from right to left, by increasing numbers starting from 2
+        for (int i = cpfList.length - 1; i >= 2; i--) {
+            cpfList[i] = String.valueOf(Integer.parseInt(cpfList[i])*multiplier);
+            multiplier++;
+            listaImprimida += " - "+cpfList[i];
         }
-        return cpfList[7];
+        return listaImprimida;
+        // resultado esperado: 10	9	8	28	24	20	28	21	14
     }
 }
