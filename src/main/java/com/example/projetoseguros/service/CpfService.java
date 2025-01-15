3ce249579462a7f0088054f3ee6e051a922602c7
Biancavitoriav <bianca.veloso@germinare.org.br>
2025-01-14 22:11:26 -0300

    conta da verificacao do segundo digito do cpf

--- src/main/java/com/example/projetoseguros/service/CpfService.java
+++ src/main/java/com/example/projetoseguros/service/CpfService.java
@@ -4,6 +4,9 @@ import com.example.projetoseguros.repositories.CpfRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @Service
 @AllArgsConstructor
 public class CpfService {
@@ -15,16 +18,22 @@ public class CpfService {
         boolean error = false;
         int multiplier = 2;
         int sum = 0;
+        double firstDigitFounded;
+        int secondDigitFounded;
+
 
         if (cpf.length() != 11) {
             error = true;
         }
 
         //inserting each cpf element in a list
-        String[] cpfList = new String[cpf.length()];
+        List<String> cpfList = new ArrayList<String>();
         for (int i = 0; i < cpf.length(); i++) {
-            cpfList[i] = String.valueOf(cpf.charAt(i));
+            cpfList.add(String.valueOf(cpf.charAt(i)));
         }
+
+        int firstDigit = Integer.parseInt(cpfList.remove(10));
+        int secondDigit = Integer.parseInt(cpfList.remove(9));
 
         //multiplying the numbers,
         //from right to left, by increasing numbers starting from 2
@@ -32,13 +41,31 @@ public class CpfService {
         for (int i = 0; i < cpf.length(); i++) {
             cpfTemporaryList[i] = String.valueOf(cpf.charAt(i));
         }
-        for (int i = cpfList.length - 1; i >= 0; i--) {
+        for (int i = cpfList.size() - 1; i >= 0; i--) {
             sum += (Integer.parseInt(cpfTemporaryList[i])*multiplier);
             multiplier++;
         }
-        double division = sum/11;
-        
-        return String.valueOf(sum);
-        // resultado esperado 162
+        if (sum % 11 < 2) {
+            firstDigitFounded = 0;
+        }else {
+            firstDigitFounded = 11 - (sum % 11);
+        }
+
+        cpfList.add(String.valueOf(firstDigitFounded));
+        for (int i = 0; i < cpf.length(); i++) {
+            cpfTemporaryList[i] = String.valueOf(cpf.charAt(i));
+        }
+        cpfTemporaryList[cpfList.size() - 1] = String.valueOf((int) firstDigitFounded);
+        for (int i = cpfList.size() - 1; i >= 0; i--) {
+            sum += (Integer.parseInt(cpfTemporaryList[i])*multiplier);
+            multiplier++;
+        }
+        if (sum % 11 < 2) {
+            secondDigitFounded = 0;
+        }else {
+            secondDigitFounded = 11 - (sum % 11);
+        }
+        return String.valueOf(secondDigitFounded);
+        // resultado esperado 5
     }
 }
